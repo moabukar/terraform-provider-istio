3a9e51c2f1080abbf62ef994372d01fe76a9d2e2
moabukar <mo-abukar10@hotmail.com>
2023-10-12 17:19:30 +0100

    add examples for virtual service

---
+++ examples/main.tf
@@ -0,0 +1,25 @@
+provider istio {
+  config_path = "/Users/mohamed/.kube/config"
+}
+
+resource "istio_virtual_service" "test"{
+  metadata {
+    name = "istio-example"
+    namespace = "test"
+  }
+
+  spec {
+    hosts = [
+      "test.prod.svc.cluster.local",
+    ]
+    http  {
+        name = "test-route"
+        route  {
+            destination {
+              host = "test.prod.svc.cluster.local"
+              subset = "v1"
+            }
+          }
+       }
+    }
+}
